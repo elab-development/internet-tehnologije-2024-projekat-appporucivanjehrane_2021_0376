f247eeaa7b89a30c8ffeda3e3e3d930f343991ae
veljovicm02 <veljovicm02@gmail.com>
2025-09-13 17:02:04 +0200

    models

---
+++ food-ordering/app/Models/MenuItem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MenuItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'category',
+        'availability'
+    ];
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
