446b917e2e1af26258e4cb6b0004bc43fe930192
fgordic <fgordic02@gmail.com>
2025-09-15 18:24:49 +0200

    api-update

--- food-ordering/routes/api.php
+++ food-ordering/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Authentication\AuthController;
+use App\Http\Controllers\MenuItemController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/menu-items', [MenuItemController::class, 'index']);
+Route::get('/menu-items/search', [MenuItemController::class, 'searchMenu']);
+Route::get('/menu-items/{id}', [MenuItemController::class, 'show']);
+
+Route::get('/orders', [OrderController::class, 'index']);
+Route::get('/orders/{id}', [OrderController::class, 'show']);
+
+Route::get('/order-items', [OrderItemController::class, 'index']);
+Route::get('/order-items/{id}', [OrderItemController::class, 'show']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('menu-items', MenuItemController::class)
+        ->only(['store', 'update', 'destroy']);
+    Route::resource('orders', OrderController::class)
+        ->only(['store', 'update', 'destroy']);
+
+    Route::post('/order-items', [OrderItemController::class, 'storeOrUpdate']);
+    Route::delete('/order-items/{id}', [OrderItemController::class, 'destroy']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
