a1ea105b8e79fc957619b17f6fb4d36d7b4c31d6
fgordic <fgordic02@gmail.com>
2025-09-20 23:45:09 +0200

    aFood - Auth Store

---
+++ food-ordering-projekat/frontend/src/store/authStore.tsx
@@ -0,0 +1,210 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from "axios";
+import { create } from "zustand";
+import { toast } from "react-toastify";
+
+import { Customer, DBRestaurant, User } from "../lib/TypesData";
+
+// API URLs
+const API_URL_USERS = "http://localhost:8000/api/users";
+const API_URL_CUSTOMERS = "http://localhost:8000/api/customers";
+const API_URL_RESTAURANTS = "http://localhost:8000/api/restaurants";
+
+interface AuthState {
+  user: User | null;
+  customerData: Customer | null;
+  restaurantData: DBRestaurant | null;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+  isCheckingAuth: boolean;
+  error: string | null;
+
+  registerCustomer: (
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+  ) => Promise<void>;
+  registerRestaurant: (
+    email: string,
+    password: string,
+    name: string,
+  ) => Promise<void>;
+  loginCustomer: (email: string, password: string) => Promise<void>;
+  loginRestaurant: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+  checkAuth: () => Promise<void>;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  customerData: null,
+  restaurantData: null,
+  isAuthenticated: false,
+  error: null,
+  isLoading: false,
+  isCheckingAuth: true,
+
+  // REGISTER CUSTOMER
+  registerCustomer: async (
+    email: string,
+    password: string,
+    firstName: string,
+    lastName: string,
+  ) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL_CUSTOMERS}/register`, {
+        email,
+        password,
+        firstName,
+        lastName,
+      });
+
+      set({
+        user: response.data.customer.user,
+        customerData: response.data.customer,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error: any) {
+      toast.error(
+        error?.response?.data?.message ||
+          "Something went wrong while registering the customer",
+      );
+      set({ isLoading: false });
+      throw error;
+    }
+  },
+
+  // LOGIN CUSTOMER
+  loginCustomer: async (email: string, password: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL_CUSTOMERS}/login`, {
+        email,
+        password,
+      });
+
+      set({
+        user: response.data.customer.user,
+        customerData: response.data.customer,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error: any) {
+      toast.error(
+        error?.response?.data?.message ||
+          "Something went wrong while loggin in the customer",
+      );
+      set({ isLoading: false });
+      throw error;
+    }
+  },
+
+  // REGISTER RESTAURANT
+  registerRestaurant: async (email: string, password: string, name: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL_RESTAURANTS}/register`, {
+        email,
+        password,
+        name,
+      });
+
+      set({
+        user: response.data.restaurant.user,
+        restaurantData: response.data.restaurant,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error: any) {
+      toast.error(
+        error?.response?.data?.message ||
+          "Something went wrong while registering the restaurant",
+      );
+      set({ isLoading: false });
+      throw error;
+    }
+  },
+
+  // LOGIN RESTAURANT
+  loginRestaurant: async (email: string, password: string) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL_RESTAURANTS}/login`, {
+        email,
+        password,
+      });
+
+      set({
+        user: response.data.restaurant.user,
+        restaurantData: response.data.restaurant,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error: any) {
+      toast.error(
+        error?.response?.data?.message ||
+          "Something went wrong while logging in the restaurant",
+      );
+      set({ isLoading: false });
+      throw error;
+    }
+  },
+
+  // LOGOUT
+  logout: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      await axios.post(`${API_URL_USERS}/logout`);
+
+      set({
+        user: null,
+        customerData: null,
+        restaurantData: null,
+        isAuthenticated: false,
+        isLoading: false,
+      });
+    } catch (error: any) {
+      toast.error(
+        error?.response?.data?.message ||
+          "Something went wrong while logging out",
+      );
+      set({ isLoading: false });
+      throw error;
+    }
+  },
+
+  // CHECK AUTH
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
+
+    try {
+      const response = await axios.get(`${API_URL_USERS}/check-auth`);
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isCheckingAuth: false,
+      });
+
+      if (response.data.user.role === "customer") {
+        set({
+          customerData: response.data.additionalData,
+        });
+      } else if (response.data.user.role === "restaurant") {
+        set({
+          restaurantData: response.data.additionalData,
+        });
+      }
+
+      // eslint-disable-next-line no-unused-vars
+    } catch (error) {
+      set({
+        error: null,
+        isAuthenticated: false,
+        isCheckingAuth: false,
+      });
+    }
+  },
+}));
